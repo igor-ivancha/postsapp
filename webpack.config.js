bc80f888d23f42a033d6ce4bc529a9f867c923f7
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-05 11:16:38 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          query: {
+            presets: [['es2015', {modules: false}], 'react', 'stage-1']
+          }
+        }
+      ]
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
