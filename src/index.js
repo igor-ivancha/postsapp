898dd6bf193d04081fdaf24c3c4920b9b0cfc871
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-06 10:08:57 +0300

    add react-router-dom and route configuration

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter as Router, browserHistory } from 'react-router-dom';
 import reducers from './reducers';
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      {routes}
+    </Router>
   </Provider>
   , document.querySelector('.container'));
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router-dom';
+import App from './components/app';
+
+export default (
+  <Route exact path="/" component={App} />
+);
