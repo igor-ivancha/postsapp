2f30630936f29c39f2625bf91b854fb0df85430e
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-06 12:41:54 +0300

    create posts_index component

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,8 @@ import React from 'react';
 import { Route, IndexRoute, Switch } from 'react-router-dom';
 import App from './components/app';
 
-const Greeting = () => {
-  return (
-    <div>Hi there!</div>
-  )
-}
-
 export default (
   <Switch>
     <Route exact path="/" component={App} />
-    <Route path="/greet" component={Greeting} />
-    <Route path="/greet2" component={Greeting} />
-    <Route path="/greet3" component={Greeting} />
   </Switch>
 );
