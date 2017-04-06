15e1530f61201bcdb41754736a29ee8df081689b
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-06 12:08:17 +0300

    switch routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router-dom';
+import { Route, IndexRoute, Switch } from 'react-router-dom';
 import App from './components/app';
 
+const Greeting = () => {
+  return (
+    <div>Hi there!</div>
+  )
+}
+
 export default (
-  <Route exact path="/" component={App} />
+  <Switch>
+    <Route exact path="/" component={App} />
+    <Route path="/greet" component={Greeting} />
+    <Route path="/greet2" component={Greeting} />
+    <Route path="/greet3" component={Greeting} />
+  </Switch>
 );
