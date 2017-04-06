ec8e1d7b07fff4615c98516312604559ce501c78
igor-ivancha <sg.igor.iv@gmail.com>
2017-04-06 16:44:31 +0300

    create posts_new component, add route for it

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from './components/app';
+import PostsNew from './components/posts_new';
 
 export default (
   <Switch>
     <Route exact path="/" component={App} />
+    <Route path="/posts/new" component={PostsNew} />
   </Switch>
 );
